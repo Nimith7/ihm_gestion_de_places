4341484a00ed59db4639add2aa40061cb3e59b29
Nicolas Calvi <nicolascalvi.35@gmail.com>
2022-03-29 15:16:27 +0200

    Début du projet

---
+++ ihm_places.php
@@ -0,0 +1,98 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Borne Tactile Festival - Places de spectacles</title>
+    <link rel="stylesheet" href="ihm_places.css">
+</head>
+<body>
+<div class="column">
+    <!-- Header -->
+    <div class="header">
+        <h1>ACTUELLEMENT AU FESTIVAL</h1>
+    </div>
+    <!-- Main -->
+    <div class="main">
+        <!---->
+        <div class="main-titre row">
+            <h2>Spectacles et Évènements</h2>
+            <h3>/</h3>
+            <h3>Étonnants Voyageurs</h3>
+            <button>Changer de festival</button>
+        </div>
+        <!---->
+        <nav class="filtres">
+            <button>Spectacle</button>
+            <button>Théatre</button>
+            <button>Film</button>
+        </nav>
+        <!---->
+        <div class="liste-event row">
+            <div class="card-event">
+                <div class="affiche-event">
+                    <a>
+                        <div class="fakeimg" style="    width: 300px; height: 400px;">Image</div>
+                    </a>
+                </div>
+                <div>
+                    <h3>Titre</h3>
+                </div>
+            </div>
+            <!---->
+            <div class="card-event">
+                <div class="affiche-event">
+                    <a>
+                        <div class="fakeimg" style="    width: 300px; height: 400px;">Image</div>
+                    </a>
+                </div>
+                <div>
+                    <h3>Titre</h3>
+                </div>
+            </div>
+            <!----><div class="card-event">
+                <div class="affiche-event">
+                    <a>
+                        <div class="fakeimg" style="    width: 300px; height: 400px;">Image</div>
+                    </a>
+                </div>
+                <div>
+                    <h3>Titre</h3>
+                </div>
+            </div>
+            <!----><div class="card-event">
+                <div class="affiche-event">
+                    <a>
+                        <div class="fakeimg" style="    width: 300px; height: 400px;">Image</div>
+                    </a>
+                </div>
+                <div>
+                    <h3>Titre</h3>
+                </div>
+            </div>
+            <!----><div class="card-event">
+                <div class="affiche-event">
+                    <a>
+                        <div class="fakeimg" style="    width: 300px; height: 400px;">Image</div>
+                    </a>
+                </div>
+                <div>
+                    <h3>Titre</h3>
+                </div>
+            </div>
+            <!---->
+        </div>
+    </div>
+    <!-- Footer -->
+    <div class="footer">
+        <h3>PROJET BORNE TACTILE POUR FESTIVALS</h3>
+        <p>IHM Gestion de places réalisé par Nicolas Calvi</p>
+    </div>
+    <!---->
+</div>
+<! -- 𝖓𝖎𝖒𝖎𝖙𝖍 𓆩*𓆪 Newt._.Castel -->
+</body>
+</html>
+
