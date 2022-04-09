aff5b5e72e710125a89e18db3150bf142e363639
Nicolas Calvi <nicolascalvi.35@gmail.com>
2022-04-09 17:43:10 +0200

    Modif Insert.php

--- form-films-etonnants-voyageurs/insert.php
+++ form-films-etonnants-voyageurs/insert.php
@@ -2,7 +2,7 @@
 // include_once './config/database.php';
 $hostname = "localhost";
 $username = "root";
-$password = "bonjour";
+$password = "root";
 $db = "gestionplaces";
 
 $dbconnect = mysqli_connect($hostname, $username, $password, $db);
@@ -12,17 +12,19 @@ if ($dbconnect->connect_error) {
 }
 
 if (isset($_POST['submit'])) {
+    $titre_event = $_POST['film'];
     $nom = $_POST['nom'];
     $email = $_POST['email'];
     $tel = $_POST['tel'];
     $places = $_POST['places'];
+    $event_id_sur_cette_page = $_POST['event_id'];
 
-    $sql = "INSERT INTO clients ( nom, email, tel, nb_place_res) VALUES ('$nom','$email','$tel','$places')";
+    $sql = "INSERT INTO clients ( nom, email, tel, nb_place_res, titre_event, event_id) VALUES ('$nom','$email','$tel','$places', '$titre_event', '$event_id_sur_cette_page')";
 
     if (!mysqli_query($dbconnect, $sql)) {
-        echo('Une erreur s’est produite. Votre réservation n’a pas été soumise.');
+        die('Une erreur s’est produite. Votre réservation n’a pas été soumise.');
     } else {
-        echo "Merci pour votre réservation.";
+        echo "Merci pour votre réservation. ";
     }
 }
 ?>
--- form-films-quai-des-bulles/insert.php
+++ form-films-quai-des-bulles/insert.php
@@ -11,15 +11,15 @@ if ($dbconnect->connect_error) {
     die("Échec de la connexion à la base de données : " . $dbconnect->connect_error);
 }
 
-$event_id_sur_cette_page = 2;
-
 if (isset($_POST['submit'])) {
+    $titre_event = $_POST['film'];
     $nom = $_POST['nom'];
     $email = $_POST['email'];
     $tel = $_POST['tel'];
     $places = $_POST['places'];
+    $event_id_sur_cette_page = $_POST['event_id'];
 
-    $sql = "INSERT INTO clients ( nom, email, tel, nb_place_res, event_id) VALUES ('$nom','$email','$tel','$places', '$event_id_sur_cette_page')";
+    $sql = "INSERT INTO clients ( nom, email, tel, nb_place_res, titre_event, event_id) VALUES ('$nom','$email','$tel','$places', '$titre_event', '$event_id_sur_cette_page')";
 
     if (!mysqli_query($dbconnect, $sql)) {
         die('Une erreur s’est produite. Votre réservation n’a pas été soumise.');
